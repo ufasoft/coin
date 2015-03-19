7bfa25b5d3d6fc07a621b9af5359438e54433914
ufasoft <dev@ufasoft.com>
2015-03-19 23:44:46 +0500

    -

---
+++ src/coin/eng/reorganize.cpp
@@ -0,0 +1,16 @@
+/*######   Copyright (c) 2011-2015 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+#                                                                                                                                     #
+# 		See LICENSE for licensing information                                                                                         #
+#####################################################################################################################################*/
+
+#include <el/ext.h>
+
+#include "coin-protocol.h"
+#include "eng.h"
+
+
+namespace Coin {
+
+
+} // Coin::
+
