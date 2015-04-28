073cafc70f08b265be53299d750d184ab88897c0
ufasoft <dev@ufasoft.com>
2015-04-29 00:27:51 +0500

    -

--- src/coin/eng/coineng.cpp
+++ src/coin/eng/coineng.cpp
@@ -1047,7 +1047,7 @@ CoinEngApp::CoinEngApp() {
 	m_internalName = "Coin";
 
 #if !UCFG_WCE
-	m_appDataDir = Environment::GetEnvironmentVariable("COIN_APPDATA");
+	m_appDataDir = explicit_cast<wstring>(Environment::GetEnvironmentVariable("COIN_APPDATA"));
 #endif
 
 #if UCFG_TRC //!!!D
