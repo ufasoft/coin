12508b73ab5bc24dc7b727f695e0476b1771e837
ufasoft <dev@ufasoft.com>
2015-04-29 16:53:59 +0500

    -

--- src/crypto/cryp/secp256k1-envelop.cpp
+++ src/crypto/cryp/secp256k1-envelop.cpp
@@ -1,7 +1,18 @@
+/*######   Copyright (c) 2015 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+#                                                                                                                                     #
+# 		See LICENSE for licensing information                                                                                         #
+#####################################################################################################################################*/
+
 #include <el/ext.h>
 
 #include "cryp-config.h"
+
 #include "secp256k1.h"
+
+#include <secp256k1/secp256k1.h>
+#include <secp256k1/util.h>
+
+#pragma warning(disable: 4319)
 
 #pragma comment(lib, "mpir")
 
@@ -12,17 +23,12 @@
 namespace Ext { namespace Crypto {
 
 static struct InitSec256 {
-	InitSec256() {
-		secp256k1_start();
-	}
-
-	~InitSec256() {
-		secp256k1_stop();
-	}
+	InitSec256() { secp256k1_start(SECP256K1_START_SIGN | SECP256K1_START_VERIFY); }
+	~InitSec256() { secp256k1_stop(); }
 } s_initSec256;
 
 Sec256Signature::Sec256Signature() {
-	secp256k1_ecdsa_sig_init(&m_sig);
+//	secp256k1_ecdsa_sig_init(&m_sig);
 }
 
 Sec256Signature::Sec256Signature(const ConstBuf& cbuf) {
@@ -30,24 +36,26 @@ Sec256Signature::Sec256Signature(const ConstBuf& cbuf) {
 }
 
 Sec256Signature::~Sec256Signature() {
-	secp256k1_ecdsa_sig_free(&m_sig);
+//	secp256k1_ecdsa_sig_free(&m_sig);
 }
 
 void Sec256Signature::Parse(const ConstBuf& cbuf) {
 	if (!secp256k1_ecdsa_sig_parse(&m_sig, cbuf.P, cbuf.Size))
-		throw CryptoException(E_EXT_Crypto, "Invalid Signature");
+		throw CryptoException(ExtErr::Crypto, "Invalid Signature");
 }
 
 void Sec256Signature::AssignCompact(const array<byte, 64>& ar) {
-    secp256k1_num_set_bin(&m_sig.r, ar.data(), 32);
-    secp256k1_num_set_bin(&m_sig.s, ar.data()+32, 32);
+	int over1, over2;
+	secp256k1_scalar_set_b32(&m_sig.r, ar.data(), &over1);
+	secp256k1_scalar_set_b32(&m_sig.s, ar.data()+32, &over2);
+	ASSERT(!over1 && !over2);
 }
 
 
 array<byte, 64> Sec256Signature::ToCompact() const {
 	array<byte, 64> r;
-	secp256k1_num_get_bin(r.data(), 32, &m_sig.r);
-    secp256k1_num_get_bin(r.data()+32, 32, &m_sig.s);
+	secp256k1_scalar_get_b32(r.data(), &m_sig.r);
+	secp256k1_scalar_get_b32(r.data()+32, &m_sig.s);
 	return r;
 }
 
@@ -62,27 +70,28 @@ Blob Sec256Signature::Serialize() const {
 static Blob SerializePubKey(secp256k1_ge_t& q, bool bCompressed = false) {
 	byte buf[100];
 	int pubkeylen;
-	secp256k1_ecdsa_pubkey_serialize(&q, buf, &pubkeylen, bCompressed);
+	secp256k1_eckey_pubkey_serialize(&q, buf, &pubkeylen, bCompressed);
 	return Blob(buf, pubkeylen);
 }
 
 Blob Sec256Dsa::RecoverPubKey(const ConstBuf& hash, const Sec256Signature& sig, byte recid, bool bCompressed) {
-	ASSERT(recid < 3);
+	ASSERT(hash.Size==32 && recid < 3);
 
-	secp256k1_num_t m;
-    secp256k1_num_set_bin(&m, hash.P, hash.Size);
+	secp256k1_scalar_t m;
+	int over;
+	secp256k1_scalar_set_b32(&m, hash.P, &over);
+	ASSERT(!over);
     secp256k1_ge_t q;
 	Blob r(nullptr);
 	if (secp256k1_ecdsa_sig_recover(&sig.m_sig, &q, &m, recid)) {
 		r = SerializePubKey(q, bCompressed);
-		secp256k1_num_free(&m);
 	}
 	return r;
 }
 
 void Sec256Dsa::ParsePubKey(const ConstBuf& cbuf) {
-	if (!secp256k1_ecdsa_pubkey_parse(&m_pubkey, cbuf.P, cbuf.Size))
-		throw CryptoException(E_EXT_Crypto, "Invalid PubKey");
+	if (!secp256k1_eckey_pubkey_parse(&m_pubkey, cbuf.P, cbuf.Size))
+		throw CryptoException(ExtErr::Crypto, "Invalid PubKey");
 }
 
 Blob Sec256Dsa::SignHash(const ConstBuf& hash) {
@@ -90,11 +99,12 @@ Blob Sec256Dsa::SignHash(const ConstBuf& hash) {
 }
 
 bool Sec256Dsa::VerifyHashSig(const ConstBuf& hash, const Sec256Signature& sig) {
-	secp256k1_num_t msg;
-    secp256k1_num_set_bin(&msg, hash.P, hash.Size);
-	int r = secp256k1_ecdsa_sig_verify(&sig.m_sig, &m_pubkey, &msg);
-    secp256k1_num_free(&msg);
-	return r;
+	ASSERT(hash.Size==32);
+	secp256k1_scalar_t msg;
+	int over;
+	secp256k1_scalar_set_b32(&msg, hash.P, &over);
+	ASSERT(!over);
+	return secp256k1_ecdsa_sig_verify(&sig.m_sig, &m_pubkey, &msg);
 }
 
 
