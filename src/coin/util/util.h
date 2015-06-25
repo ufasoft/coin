919efcf3f5e9acfa6975e0712b7b90ed2262dcdd
ufasoft <dev@ufasoft.com>
2015-06-25 19:23:42 +0500

    -

--- src/coin/util/util.h
+++ src/coin/util/util.h
@@ -8,6 +8,18 @@
 #include <el/crypto/hash.h>
 using namespace Ext::Crypto;
 
+#ifndef UCFG_COIN_ECC
+#	define UCFG_COIN_ECC 'S'			// Secp256k1
+#endif
+
+#ifndef UCFG_COIN_USE_OPENSSL
+#	define UCFG_COIN_USE_OPENSSL 1
+#endif
+
+#if UCFG_COIN_ECC!='S' && UCFG_COIN_USE_OPENSSL
+#	include <el/crypto/ecdsa.h>
+#endif
+
 #ifndef UCFG_COIN_PUBKEYID_36BITS
 #	define UCFG_COIN_PUBKEYID_36BITS 0
 #endif
@@ -77,8 +89,14 @@ ENUM_CLASS(HashAlgo) {
 HashAlgo StringToAlgo(RCString s);
 String AlgoToString(HashAlgo algo);
 
-class HashValue : public std::array<byte, 32>, totally_ordered<HashValue> {
+class HashValue : totally_ordered<HashValue> {
 public:
+	typedef byte *iterator;
+	typedef const byte *const_iterator;
+
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
 	HashValue() {
 		memset(data(), 0, 32);
 	}
@@ -90,21 +108,48 @@ public:
 	HashValue(const hashval& hv);
 	HashValue(const ConstBuf& mb);
 	explicit HashValue(RCString s);
+
+	static const HashValue& Null();
+
+	byte *data() { return (byte*)m_data; }
+	const byte *data() const { return (const byte*)m_data; }
+
+	const_iterator begin() const { return data(); }
+	const_iterator end() const { return data()+32; }
+	const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+	const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+
+	ConstBuf ToConstBuf() const {
+		return ConstBuf(data(), 32);
+	}
+
 	static HashValue FromDifficultyBits(uint32_t bits);
 	uint32_t ToDifficultyBits() const;
 	static HashValue FromShareDifficulty(double difficulty, HashAlgo algo);
+
+	EXPLICIT_OPERATOR_BOOL() const {
+		return *this == Null() ? 0 : EXT_CONVERTIBLE_TO_TRUE;
+	}
+
+	bool operator!() const { return !bool(*this); }
+
+	bool operator==(const HashValue& v) const {
+		return !memcmp(data(), v.data(), 32);
+	}
 
 	bool operator<(const HashValue& v) const;
 
 	static HashValue Combine(const HashValue& h1, const HashValue& h2);
 
 	void Write(BinaryWriter& wr) const {
-		wr.Write(&_self[0], size());
+		wr.Write(data(), 32);
 	}
 
 	void Read(const BinaryReader& rd) {
-		rd.Read(&_self[0], size());
+		rd.Read(data(), 32);
 	}
+private:
+	uint64_t m_data[4];
 };
 
 
@@ -189,7 +234,7 @@ private:
 class ReducedBlockHash {
 public:
 	ReducedBlockHash(const HashValue& hash)
-		:	m_buf(hash)
+		:	m_buf(hash.ToConstBuf())
 	{
 		while (m_buf.Size > 1 && !m_buf.P[m_buf.Size-1])
 			m_buf.Size--;
@@ -210,8 +255,9 @@ String ConvertToBase58ShaSquare(const ConstBuf& cbuf);
 Blob ConvertFromBase58ShaSquare(RCString s);
 COIN_UTIL_EXPORT Blob CalcSha256Midstate(const ConstBuf& mb);
 
-COIN_UTIL_EXPORT  HashValue ScryptHash(const ConstBuf& mb);
-COIN_UTIL_EXPORT  HashValue NeoSCryptHash(const ConstBuf& mb, int profile);
+COIN_UTIL_EXPORT HashValue SHA256_SHA256(const ConstBuf& cbuf);
+COIN_UTIL_EXPORT HashValue ScryptHash(const ConstBuf& mb);
+COIN_UTIL_EXPORT HashValue NeoSCryptHash(const ConstBuf& mb, int profile);
 HashValue SolidcoinHash(const ConstBuf& cbuf);
 HashValue MetisHash(const ConstBuf& cbuf);
 COIN_UTIL_EXPORT HashValue GroestlHash(const ConstBuf& mb);
@@ -311,6 +357,13 @@ const char DEFAULT_PASSWORD_ENCRYPT_METHOD = 'B';
 
 class HasherEng : public Object {
 public:
+	byte AddressVersion, ScriptAddressVersion;
+
+	HasherEng()
+		: AddressVersion(0)
+		, ScriptAddressVersion(5) {
+	}
+
 	virtual HashValue HashBuf(const ConstBuf& cbuf);
 	virtual HashValue HashForAddress(const ConstBuf& cbuf);
 	static HasherEng *GetCurrent();
@@ -324,6 +377,113 @@ class CHasherEngThreadKeeper {
 public:
 	CHasherEngThreadKeeper(HasherEng *cur);
 	~CHasherEngThreadKeeper();
+};
+
+class PrivateKey : public CPrintable {
+public:
+	PrivateKey() {}
+	PrivateKey(const ConstBuf& cbuf, bool bCompressed);
+	explicit PrivateKey(RCString s);
+	pair<Blob, bool> GetPrivdataCompressed() const;
+	String ToString() const override;
+private:
+	Blob m_blob;
+};
+
+class CanonicalPubKey {
+	typedef CanonicalPubKey class_type;
+public:
+	Blob Data;
+
+	CanonicalPubKey() {
+	}
+
+	CanonicalPubKey(nullptr_t)
+		: Data(nullptr) {
+	}
+
+	CanonicalPubKey(const ConstBuf& cbuf)
+		: Data(cbuf) {
+	}
+
+	bool IsCompressed() const { return Data.Size == 33; }
+
+	HashValue160 get_Hash160() const {
+		return Coin::Hash160(Data);
+	}
+	DEFPROP_GET(HashValue160, Hash160);
+	
+	bool IsValid() const {
+		byte b0 = Data.constData()[0];
+		return (Data.Size == 33 && (b0==2 || b0==3)) ||
+			(Data.Size == 65 || b0==4);
+	}
+
+	Blob ToCompressed() const;
+	static CanonicalPubKey FromCompressed(const ConstBuf& cbuf);
+};
+
+class COIN_CLASS Address : public HashValue160, public CPrintable {
+public:
+	HasherEng& Hasher;
+	String Comment;
+	byte Ver;
+
+	Address(HasherEng& hasher);
+	explicit Address(HasherEng& hasher, const HashValue160& hash, RCString comment = "");
+	explicit Address(HasherEng& hasher, const HashValue160& hash, byte ver);
+	explicit Address(HasherEng& hasher, RCString s);
+
+	Address& operator=(const Address& a) {
+		if (&Hasher != &a.Hasher)
+			Throw(errc::invalid_argument);
+		HashValue160::operator=(a);
+		Comment = a.Comment;
+		Ver = a.Ver;
+		return *this;
+	}
+
+	void CheckVer(HasherEng& hasher) const;
+	String ToString() const override;
+
+	bool operator<(const Address& a) const {
+		return Ver < a.Ver ||
+			(Ver == a.Ver && memcmp(data(), a.data(), 20) < 0);
+	}
+};
+
+class KeyInfoBase {
+	typedef KeyInfoBase class_type;
+public:
+	CanonicalPubKey PubKey;
+
+#if UCFG_COIN_ECC!='S'
+	CngKey Key;
+#endif
+
+	DateTime Timestamp;
+	String Comment;
+
+	static KeyInfoBase GenRandom(bool bCompressed = true);
+
+	Blob PlainPrivKey() const;
+
+	Blob get_PrivKey() const { return m_privKey; }
+	DEFPROP_GET(Blob, PrivKey);
+
+
+	Address ToAddress() const;
+	void SetPrivData(const ConstBuf& cbuf, bool bCompressed);
+	void SetPrivData(const PrivateKey& privKey);
+	void SetKeyFromPubKey();
+	String ToString(RCString password) const;					// To WIF / BIP0038 formats
+	Blob SignHash(const ConstBuf& cbuf);
+	static bool VerifyHash(const ConstBuf& pubKey, const HashValue& hash, const ConstBuf& sig);
+	
+	Blob ToPrivateKeyDER() const;
+	static KeyInfoBase FromDER(const ConstBuf& privKey, const ConstBuf& pubKey);
+private:
+	Blob m_privKey;
 };
 
 
