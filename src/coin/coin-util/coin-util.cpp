5ebb04a94641ffc3e2132cac8ec522c922c6b1e4
ufasoft <dev@ufasoft.com>
2015-06-25 19:15:39 +0500

    -

--- src/coin/coin-util/coin-util.cpp
+++ src/coin/coin-util/coin-util.cpp
@@ -6,16 +6,31 @@
 namespace Coin {
 
 struct CoinData {
+	String Symbol;
 	byte AddressType;
+	byte ScriptAddressVersion;
 };
 
 class UtilHasherEng : public HasherEng {
 	typedef HasherEng base;
 public:
-	String Symbol;
+	const String Symbol;
 
 	UtilHasherEng(RCString symbol)
 		: Symbol(symbol) {
+		AddressVersion = Symbol == "GRS" ? 36
+			: Symbol == "LTC" ? 48
+			: Symbol == "PPC" ? 117
+			: Symbol == "DVC" ? 4
+			: Symbol == "DOGE" ? 30
+			: Symbol == "EAC" ? 93
+			: Symbol == "FTC" ? 14
+			: Symbol == "IXC" ? 138
+			: Symbol == "NMC" ? 52
+			: Symbol == "NVC" ? 8
+			: Symbol == "XPM" ? 23
+			: Symbol == "PTS" ? 56
+			: 0;
 	}	
 
 protected:
@@ -26,28 +41,35 @@ protected:
 	}
 };
 
+static HasherEng *g_hasher;
+
 class UtilApp : public CConApp {
 public:
 	void PrintUsage() {
 		cout << "Usage: " << System.get_ExeFilePath().stem() << " {-options} command args"
 			"\nOptions:\n"
-			"  -a currencyName     Bitcoin|Litecoin|Groestlcoin...\n"
+			"  -a currencyName     Bitcoin|BTC|Litecoin|LTC|Groestlcoin|GRS|...\n"
 			"  -h                  this help\n"
 			"\nCommands:\n"
-			"  pubkey-to-address <pubkey-as-hexadecimal>\n"
-			"    Outputs address of geven PublickKey\n"
-
+			"  pubkey-to-addr <pubkey-as-hexadecimal> [address-ver]\n"
+			"    Outputs address of given Public Key\n"
+			"      <address-ver> optional integer arg, by default depends on currencyName (0 for Bitcoin)"
+			"\n";
 	}
 
 	void PubKeyToAddress() {
-		if (optind < Argc) {
+		if (optind >= Argc) {
 			PrintUsage();
 			Throw(2);
 		}
-		Blob pubKey = Blob::FromHexString(Argv[optind]);
+		Blob pubKey = Blob::FromHexString(Argv[optind++]);
+		if (optind < Argc)
+			g_hasher->AddressVersion = (byte)atoi(Argv[optind++]);
+		Address addr(*g_hasher, Hash160(pubKey));
+		cout << addr << endl;
 	}
 
-	String GetCanonicCurrencyName(RCString s) {
+	String GetCurrencySymbol(RCString s) {
 		String x = s.ToUpper();
 		if (x.Right(4) == "COIN")
 			x = s.substr(0, s.length() - 4);
@@ -73,25 +95,28 @@ public:
 	void Execute() override {
 		String currency = "BITCOIN";
 
-		for (int arg; (arg = getopt(Argc, Argv, "a:"));) {
-		case 'a':
-			currency = optarg;
-			break;
-		case 'h':
-			PrintUsage();
-			return;
-
+		for (int arg; (arg = getopt(Argc, Argv, "a:h"))!=EOF;) {
+			switch (arg) {
+			case 'a':
+				currency = optarg;
+				break;
+			case 'h':
+				PrintUsage();
+				return;
+			default:
+				PrintUsage();
+				Throw(1);
+			}
 		}
-		if (optind < Argc) {
+		if (optind >= Argc) {
 			PrintUsage();
 			Throw(2);
 		}
-		HasherEng *hasher = CreateHasherEng(GetCanonicCurrencySymbol(currency));
-		CHasherEngThreadKeeper hasherKeeper(hasher);
-
-
+		g_hasher = CreateHasherEng(GetCurrencySymbol(currency));
+		CHasherEngThreadKeeper hasherKeeper(g_hasher);
+		
 		String command = Argv[optind++];
-		if (command == "pubkey-to-address")
+		if (command == "pubkey-to-addr")
 			PubKeyToAddress();
 		else {
 			PrintUsage();
@@ -100,8 +125,9 @@ public:
 	}
 
 
-} threApp;
+} theApp;
 
 } // Coin::
 
 EXT_DEFINE_MAIN(Coin::theApp)
+
