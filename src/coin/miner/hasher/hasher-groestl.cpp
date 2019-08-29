ce5414d6bc7eb40c8bf41beeb5f9985fa544eeb9
ufasoft <dev@ufasoft.com>
2019-08-28 23:13:00 -0700

    -

--- src/coin/miner/hasher/hasher-groestl.cpp
+++ src/coin/miner/hasher/hasher-groestl.cpp
@@ -1,4 +1,4 @@
-/*######   Copyright (c) 2015 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+/*######   Copyright (c) 2015-2019 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
 #                                                                                                                                     #
 # 		See LICENSE for licensing information                                                                                         #
 #####################################################################################################################################*/
@@ -18,9 +18,9 @@ public:
 	HashValue CalcHash(RCSpan cbuf) override {
 		return GroestlHash(cbuf);
 	}
-
 } g_groestlHasher;
 
-
+Hasher& GetGroestlcoinHasher() { return g_groestlHasher; }	//!!!?
 
 } // Coin::
+
